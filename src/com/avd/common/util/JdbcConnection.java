0b6d495cb6300e17cfe9d2c86731463fdb593440
AvdheshCoder <avi.gupta14@gmail.com>
2016-07-21 18:38:03 +0530

    Project Start

---
+++ src/com/avd/common/util/JdbcConnection.java
@@ -0,0 +1,85 @@
+package com.avd.common.util;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+
+
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JdbcConnection  {
+
+	@Value("${database.url}")
+	static
+	
+	String url;
+	@Value("${database.user}")
+	static
+	
+	String user;
+	@Value("${database.password}")
+	static
+	String pass;
+
+	@Value("${database.driver}")
+	static
+	
+	String driverName="com.mysql.jdbc.Driver";
+	static Connection con = null;
+
+	public Connection getConnection(String dbName, String username,
+			String password) {
+
+		try {
+			Class.forName(driverName);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			con = DriverManager.getConnection(url, username, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+	public static Connection getConnection() {
+
+		try {
+			System.out.println("driver name:"+ driverName);
+			Class.forName(driverName);
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			
+				
+
+			
+			
+			  url="jdbc:mysql://localhost/test";
+		 	  user="root"; 
+			  pass="123";
+
+			
+			/*url="jdbc:mysql://mysql.mohinimart.com/brajesh782_mohini";
+		 	  user="braje_mohini"; 
+			  pass="arun123@";
+		
+			
+			*/
+			con = DriverManager.getConnection(url, user, pass);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+}
